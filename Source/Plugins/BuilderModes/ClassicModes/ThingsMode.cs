01bcdc01b2dc94931ec3c3cadc22024c6bef5aad
wisselstem <36791160+spherallic@users.noreply.github.com>
2023-09-17 13:06:57 +0200

    Point Thing at Cursor: check for a thing's fixedrotation rather than its category's arrow setting (#957)

--- Source/Plugins/BuilderModes/ClassicModes/ThingsMode.cs
+++ Source/Plugins/BuilderModes/ClassicModes/ThingsMode.cs
@@ -1474,7 +1474,7 @@ namespace CodeImp.DoomBuilder.BuilderModes
 				foreach(Thing t in selected) 
 				{
 					ThingTypeInfo info = General.Map.Data.GetThingInfo(t.Type);
-					if(info == null || info.Category == null || info.Category.Arrow == 0)
+					if(info == null || info.FixedRotation == true)
 						continue;
 
 					int newangle = Angle2D.RealToDoom(Vector2D.GetAngle(mousemappos, t.Position) + Angle2D.PI);
@@ -1488,7 +1488,7 @@ namespace CodeImp.DoomBuilder.BuilderModes
 				foreach(Thing t in selected) 
 				{
 					ThingTypeInfo info = General.Map.Data.GetThingInfo(t.Type);
-					if(info == null || info.Category == null || info.Category.Arrow == 0)
+					if(info == null || info.FixedRotation == true)
 						continue;
 
 					int newangle = Angle2D.RealToDoom(Vector2D.GetAngle(mousemappos, t.Position));
